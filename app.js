92a00d70f7333e2ecb4201110457fab5633548b1
Yash <79582883+yash-aryan@users.noreply.github.com>
2024-06-22 22:27:23 +0530

    Setup Fresh Express Server & Connect to Database
    - Used express-generator to setup express server using ejs as the
    templating language of choice.
    - Installed some required dependencies.
    - Connected to Mongo database using MONGO_URI stored privately in .env

---
+++ app.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+// mongoose setup
+mongoose.set('strictQuery', false);
+
+// connect to database
+(async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+	} catch (err) {
+		console.log(err);
+	}
+})();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// more middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
